70f9d2d54a0c6415c9d3e744f92387f1dc77526a
amrit <bhudathokiamrit@gmail.com>
2025-05-07 22:10:03 +0545

    added routes files

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -35,5 +35,19 @@ export const register = async (req, res) => {
         res.status(500).json({ error: error.message });     
         
     }
+};
+//Login User
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) return res.status(400).json({ msg: "User does not exist." });
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET); 
+    } catch(err){
+        res.status(500).json({ error: err.message });
+    }
 }
- 
+
+ export default { register, login };
--- Backend/index.js
+++ Backend/index.js
@@ -8,8 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { register } from "module";
-import { register } from "./controllers/auth.js";
+// import authRoutes from "./routes/auth.js";
+import { register,login } from "./controllers/auth.js";
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -40,7 +40,8 @@ const storage = multer.diskStorage({
 
     // Routes with files
     app.post("/auth/register", upload.single("picture"), register); 
-
+//Routes
+app.post("/auth/login", login);
 
     // MONGOOSE SETUP
 
