ddbef6114eaf58eded5d883645c5a482d4cd1d56
loof94 <gkbiswokarma62@gmail.com>
2025-05-10 11:30:32 +0545

    Save local changes before pull

--- Backend/index.js
+++ Backend/index.js
@@ -8,14 +8,12 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { register } from "module";
-import { register } from "./controllers/auth.js";
-
+import register from "./controllers/auth.js";
+dotenv.config();
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -29,28 +27,25 @@ app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 // File Storage
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    },
-    });
-    const upload = multer({ storage });
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
-    // Routes with files
-    app.post("/auth/register", upload.single("picture"), register); 
+// Routes with files
+app.post("/auth/register", upload.single("picture"), register);
 
-
-    // MONGOOSE SETUP
-
+// MONGOOSE SETUP
 
 const PORT = process.env.PORT || 6001;
-
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
+    console.log("Connected to MongoDB");
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
   })
   .catch((error) => console.log(`${error} - Did not connect`));
-
